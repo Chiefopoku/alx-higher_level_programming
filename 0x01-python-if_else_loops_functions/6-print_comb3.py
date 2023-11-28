be0ce9de807eeedbc61f9967554aad1a98dbb7d5
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-11-28 10:39:10 -0800

    combi 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(9):
     for a in range(i + 1, 10):
         if i * 10 + a < 89:
-            print("{:d}{:d}".format(i,a), end=", ")
+            print("{:d}{:d}".format(i, a), end=", ")
 print("{:d}".format(89))
