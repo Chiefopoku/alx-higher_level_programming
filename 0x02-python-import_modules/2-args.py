15264c65958c65f86465b0515df89b58d4e225d7
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-11-30 14:03:17 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     import sys
     argLen = len(sys.argv)
-    num_arguments = len(sys.argv) - 1
+    count = len(sys.argv) - 1
 
-    if num_arguments == 0:
-        print("0 arguments.")
-    elif num_arguments == 1:
-        print("1 argument.")
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
     else:
-        print("{} arguments.".format(num_arguments))
+        print("{} arguments:".format(count))
 
     for i in range(1, argLen):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i, sys.argv[i]))
