3d8e2c245f7c2f60d2e2bff3dd1143b189c1c39b
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-11-30 16:50:23 -0800

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
 
     from magic_calculation_102 import add, sub
@@ -13,4 +12,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-
