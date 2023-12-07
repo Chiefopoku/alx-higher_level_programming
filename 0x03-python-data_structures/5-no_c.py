2310087ca8942a1278bc7036c5bbaa443c651edf
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-07 07:19:37 -0800

    remove all c from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_str = ""
 
-    for i in my_string:
-        if i != "c" and i != "C":
-            new_str += i
+    for char in my_string:
+        if char.lower() != "c":
+            new_str += char
     return new_str
