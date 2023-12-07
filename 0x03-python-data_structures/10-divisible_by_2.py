8bda5e70561beb909930c8649a6119799336cf60
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-07 13:15:00 -0800

    divide by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     new_list = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
-            new_list = True
+            new_list[count] = True
         else:
-            new_list = False
+            new_list[count] = False
     return new_list
