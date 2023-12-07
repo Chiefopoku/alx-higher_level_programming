e0cecc5f0cece0e7abb4164d67f2913f2e144845
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-07 10:57:25 -0800

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,9 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     first_char = sentence[0] 
-    if lenght < 0:
-        first_char = None
-    else:
+    if lenght > 0:
         first_char = sentence[0]
+    else:
+        first_char = None
     tup = lenght, first_char
     return tup
