ef505054f88e417db85267106cb3b75c3bc12fa9
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-07 09:05:29 -0800

    remove all c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def no_c(my_string):
     new_str = ""
 
-    for i in my_string:
-        if (i != 'c') and (i != 'C'):
-            new_str += i
+    for char in my_string:
+        if char.lower() != 'c':
+            new_str += char
     return new_str
