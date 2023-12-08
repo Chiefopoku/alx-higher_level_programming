a1dc0936487262d9b4f7d3220f8d9e9f5573ac25
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-08 14:50:19 -0800

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+
+    for i, row in enumerate(new_matrix):
+        for j, col in enumerate(new_matrix):
+            new_matrix[i][j] = row[j] ** 2
+    return new_matrix
