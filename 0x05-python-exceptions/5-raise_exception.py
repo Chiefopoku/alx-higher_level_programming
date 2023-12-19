1c93c2d4a6cf66d4211855890dcf28568da1e187
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-12-18 16:11:20 -0800

    raise exceptions

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-raise_exception = __import__('5-raise_exception').raise_exception
+raise_exception = __import__('5-raise_exception').raise_exception:
 
 try:
     raise_exception()
